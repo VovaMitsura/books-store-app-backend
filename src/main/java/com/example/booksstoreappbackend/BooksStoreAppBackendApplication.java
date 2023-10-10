ee4735c31985ab2d2a4b83cb31abc6d797c79a6c
Vova Mitsura <vovamitsura@gmail.com>
2023-10-10 20:45:01 +0300

    Init: initial commit

---
+++ src/main/java/com/example/booksstoreappbackend/BooksStoreAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksstoreappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksStoreAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksStoreAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/booksstoreappbackend/BooksStoreAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksstoreappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksStoreAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
